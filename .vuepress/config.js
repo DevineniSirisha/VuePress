7377438bbbeb027c0c31c390b8c4c3cf7cddf71a
Devineni,Sai Sirisha <S531367@nwmissouri.edu>
2019-03-03 21:37:21 -0600

    initial

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports={
+    title:'Bearcats',
+    description:'Northwest Missouri State University',
+    themeConfig: {
+        nav:[
+            {text: 'Home', link:'/'},
+            {text: 'Guide', link:'/'},
+            {text: 'About me', link:'/'},
+        ]
+    }
+}
